56d39933c91dd072be927c204100fcabe7ace59f
Oliver Knight <oliver.knight@charite.de>
2024-03-13 21:57:01 +0100

    Fixed curl issue

--- first_time_setup.sh
+++ first_time_setup.sh
@@ -14,7 +14,9 @@ mv .local ${bin_folder} && ln -s ${bin_folder}/.local ${HOME}/.local
 mkdir -p ${HOME}/ondemand
 mv ondemand ${bin_folder} && ln -s ${bin_folder}/ondemand ${HOME}/ondemand
 
-echo "Do you want to install miniconda, for building environments such as for RStudio? (y/n)"
+echo ""
+echo "Do you want to install miniconda? (y/n)"
+echo ""
 read -r choice
 
 while [[ ! $choice =~ ^[YyNn]$ ]]; do
@@ -22,7 +24,6 @@ while [[ ! $choice =~ ^[YyNn]$ ]]; do
     read -r choice
 done
 
-# Process choices
 if [ "$choice" = "Y" ] || [ "$choice" = "y" ]; then
 
     cd ${bin_folder}
@@ -49,22 +50,25 @@ channel_priority: flexible' > ${HOME}/.condarc
 
 fi
 
-echo "Do you want to create R_4.2.3? (y/newname/n)"
+echo ""
+echo "Do you want to create the R environment R_4.2.3? (y/newname/n)"
+
 read -r choice
 
-# Validate user input
+echo ""
+
 while [[ ! $choice =~ ^[YyNn]$ && $choice != "newname" ]]; do
-    echo "Invalid input. Please enter Y, N, or newname"
+    echo "Invalid input. Please enter y, n, or newname"
     read -r choice
 done
 
 environment=$HOME/group/work/bin/source/R_4.2.3.yml
-# Process user choice
+
 if [[ $choice =~ ^[Yy]$ ]]; then
-    echo ""
-    echo "Creating a new environment for RStudio. This might take a while..."
-    echo ""
     env_name='R_4.2.3'
+    echo ""
+    echo "Creating ${env_name}, this might take a while..."
+    echo ""
     conda env create -f $environment
 elif [[ $choice == "newname" ]]; then
     echo ""
@@ -74,29 +78,30 @@ elif [[ $choice == "newname" ]]; then
     cp "$environment" "${TMPDIR}/${env_name}.yml"
 
     echo ""
-    echo "Creating a new environment named '${env_name}' for RStudio..."
+    echo "Creating ${env_name}..."
     echo ""
 
     sed -i "1s/.*/name: ${env_name}/" "${TMPDIR}/${env_name}.yml"
 
     conda env create -f "${TMPDIR}/${env_name}.yml"
 
-    echo "New environment '${env_name}' created."
+    echo "${env_name} created"
 
 elif [[ $choice =~ ^[Nn]$ ]]; then
-    echo "Not creating a new environment for RStudio."
+    echo "Not creating an R environment"
 fi
 
+echo ""
 echo "Do you want to create a reticulate environment for R? (y/n)"
 read -r choice
 
-# Validate user input
+echo ""
+
 while [[ ! $choice =~ ^[YyNn] ]]; do
     echo "Invalid input. Please enter y or n"
     read -r choice
 done
 
-# Process user choice
 if [[ $choice =~ ^[Yy]$ ]]; then
     echo ""
     echo "Creating r-reticulate, this might take a while..."
@@ -107,23 +112,23 @@ elif [[ $choice =~ ^[Nn]$ ]]; then
 fi
 
 if [[ -d ${HOME}/ondemand/dev ]]; then
-    rm -r ${HOME}/ondemand/dev
+    rm -rf ${HOME}/ondemand/dev
 fi
 
-echo "Do you want to correct OnDemand for RStudio? (y/n)"
+echo "Do you want to correct the OnDemand repo (recommended)? (y/n)"
 read -r choice
+echo ""
 
 while [[ ! $choice =~ ^[YyNn]$ ]]; do
     echo "Invalid input. Please enter y or n"
     read -r choice
 done
 
-# Process choices
 if [ "$choice" = "Y" ] || [ "$choice" = "y" ]; then
     mkdir -p ${HOME}/ondemand/dev
     cd ${HOME}/ondemand/dev
-    git clone https://github.com/ollieeknight/ondemand-rstudio-server
-    source $HOME/work/bin/miniconda3/etc/profile.d/conda.sh
+    git clone https://github.com/ollieeknight/ondemand-rstudio-server > /dev/null
+    source ${bin_folder}/miniconda3/etc/profile.d/conda.sh
     conda activate
     miniconda_dir=$(echo $PATH | grep -o "$HOME/[^:]*miniconda3" | head -n 1)
     line_number=$(grep -n 'LD_LIBRARY_PATH=/usr/lib64/' ${HOME}/ondemand/dev/ondemand-rstudio-server/template/script.sh.erb | cut -d ":" -f 1)
